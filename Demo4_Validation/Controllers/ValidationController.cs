6c24e9b8994ef1aa2be602caeae88f73b7a87862
Annie <annarohanbabu@gmail.com>
2021-04-19 10:40:04 +0530

    project commit

---
+++ Demo4_Validation/Controllers/ValidationController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Demo4_Validation.Models;
+namespace Demo4_Validation.Controllers
+{
+    public class ValidationController : Controller
+    {
+        // GET: Validation
+        [HttpGet]
+        public ActionResult ValidationIndex()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ValidationIndex(Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.status = "Success";
+            }
+            else
+            {
+                ViewBag.status = "Fail";
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create([Bind(Include ="Name,Age")]Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.status = "Success";
+            }
+            else
+            {
+                ViewBag.status = "Fail";
+            }
+            return View();
+        }
+    }
+}
---
+++ Demo4_Validation/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace Demo4_Validation.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        [Required]
+        [StringLength(12)]
+        public string Name { get; set; }
+
+        [Range(10, 20)]
+        public int Age { get; set; }
+    }
+}
